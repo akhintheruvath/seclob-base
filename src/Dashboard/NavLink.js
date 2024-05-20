9c741bbdeb9ea9c6e955db0c1e51e904c2695e8f
Akhin <akhintheruvath2827@gmail.com>
2024-05-20 14:53:10 +0530

    update in active link color and opacity

--- src/Dashboard/NavLink.js
+++ src/Dashboard/NavLink.js
@@ -5,7 +5,7 @@ export const NavLink = ({ to, active, children }) => {
   return (
     <Link
       to={to}
-      className={`text-[#030229] opacity-50 font-nunito font-semibold flex items-center h-11 mb-6 w-full cursor-pointer ${active && 'text-[#605BFF] opacity-100'}`}
+      className={`font-nunito font-semibold flex items-center h-11 mb-6 w-full cursor-pointer ${active ? 'text-[#605BFF] opacity-100' : 'text-[#030229] opacity-50'}`}
     >
       {children}
     </Link>
