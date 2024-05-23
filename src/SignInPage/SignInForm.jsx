593e2f733d5ec5dcd43df74343a89b9fa3082b9a
Akhin <akhintheruvath2827@gmail.com>
2024-05-23 22:55:30 +0530

    Sign in page design updates

--- src/SignInPage/SignInForm.jsx
+++ src/SignInPage/SignInForm.jsx
@@ -11,13 +11,13 @@ export const SignInForm = () => {
 
   return (
     <div>
-      <div className='px-9 py-10 bg-white rounded-2xl'>
+      <div className='p-9 bg-white rounded-2xl'>
          <form>
             <label className='font-lato text-base' htmlFor="email">Email address</label><br />
             <input className='w-full mt-3 mb-5 font-lato bg-[#F5F5F5] focus:bg-[#EAEAEA] focus:outline-none p-3 rounded-xl text-base' type="email" id="email" name="email" placeholder='johndoe@gmail.com' /><br />
             <label className='font-lato text-base' htmlFor="password">Password</label><br />
             <input className='w-full mt-3 font-lato bg-[#F5F5F5] focus:bg-[#EAEAEA] focus:outline-none p-3 rounded-xl text-base' type="password" id="password" name="password" /><br /><br />
-            <a href='' className='font-lato text-[#346BD4] text-base'>Forgot password?</a>
+            <a href='' onClick={(e) => e.preventDefault()} className='font-lato text-[#346BD4] text-base'>Forgot password?</a>
             <button
                className='bg-[#605BFF] w-full flex items-center justify-center p-2 mt-6 rounded-xl font-montserrat text-white font-bold'
                onClick={signin}
--- src/SignInPage/SignInPage.jsx
+++ src/SignInPage/SignInPage.jsx
@@ -32,15 +32,15 @@ const SignInPage = () => {
           <p className='font-lato text-black text-base mt-3 mb-6'>Sign in to your account</p>
           <div className='flex items-center mb-6'>
             <div className='text-xs text-[#858585] rounded-xl bg-white flex items-center px-6 py-2 mr-7'>
-              <span className='mr-3'><GoogleLogo /></span>Sign in with Google
+              <GoogleLogo /><span className='ml-4 mr-2'>Sign in with Google</span>
             </div>
             <div className='text-xs text-[#858585] rounded-xl bg-white flex items-center px-6 py-2'>
-              <span className='mr-3'><AppleLogo /></span>Sign in with Apple
+              <AppleLogo /><span className='ml-4 mr-2'>Sign in with Apple</span>
             </div>
           </div>
           <SignInForm />
           <div className='w-full flex justify-center mt-5'>
-            <p className='font-lato text-base text-[#858585]'>Don't have an account? <a href='' className='text-[#346BD4]'>Register here</a></p>
+            <p className='font-lato text-base text-[#858585]'>Don't have an account? <a href='' onClick={(e) => e.preventDefault()} className='text-[#346BD4]'>Register here</a></p>
           </div>
         </div>
       </div>
