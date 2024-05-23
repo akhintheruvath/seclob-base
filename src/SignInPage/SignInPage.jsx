6cc75a8222d034ceebeeb8e0bbbe9644441e21bf
Akhin <akhintheruvath2827@gmail.com>
2024-05-24 00:37:07 +0530

    responsiveness related changes

--- src/SignInPage/SignInPage.jsx
+++ src/SignInPage/SignInPage.jsx
@@ -12,7 +12,7 @@ import './SignInPage.css';
 const SignInPage = () => {
   return (
     <div className="flex bg-[#F8FAFF]">
-      <div id='left-div' className="flex flex-col items-center justify-between py-16 h-screen w-1/2 bg-[#605BFF]">
+      <div id='left-div' className="hidden md:flex flex-col items-center justify-between py-16 h-screen w-1/2 bg-[#605BFF]">
         <div className='ml-36 flex w-full items-start'><BaseLogoSignIn /></div>
         <div>
           <h1 className='font-montserrat font-bold text-white text-7xl mr-20'>BASE</h1>
@@ -26,20 +26,20 @@ const SignInPage = () => {
           </div>
         </div>
       </div>
-      <div className="w-1/2 flex items-center h-screen pl-24">
+      <div className="w-full md:w-1/2 flex justify-center md:justify-normal items-center h-screen md:pl-24">
         <div>
-          <h3 className='font-montserrat font-bold text-black text-4xl'>Sign In</h3>
-          <p className='font-lato text-black text-base mt-3 mb-6'>Sign in to your account</p>
+          <h3 className='font-montserrat font-bold text-black text-2xl md:text-4xl'>Sign In</h3>
+          <p className='font-lato text-black text-xs md:text-base mt-3 mb-6'>Sign in to your account</p>
           <div className='flex items-center mb-6'>
-            <div className='text-xs text-[#858585] rounded-xl bg-white flex items-center px-6 py-2 mr-7'>
-              <GoogleLogo /><span className='ml-4 mr-2'>Sign in with Google</span>
+            <div className='text-[10px] md:text-xs text-[#858585] rounded-xl bg-white flex items-center px-6 py-2 mr-7'>
+              <GoogleLogo /><span className='ml-4 md:mr-2 text-nowrap'>Sign in with Google</span>
             </div>
-            <div className='text-xs text-[#858585] rounded-xl bg-white flex items-center px-6 py-2'>
-              <AppleLogo /><span className='ml-4 mr-2'>Sign in with Apple</span>
+            <div className='text-[10px] md:text-xs text-[#858585] rounded-xl bg-white flex items-center px-6 py-2'>
+              <AppleLogo /><span className='ml-4 md:mr-2 text-nowrap'>Sign in with Apple</span>
             </div>
           </div>
           <SignInForm />
-          <div className='w-full flex justify-center mt-5'>
+          <div className='flex justify-center mt-5'>
             <p className='font-lato text-base text-[#858585]'>Don't have an account? <a href='' onClick={(e) => e.preventDefault()} className='text-[#346BD4]'>Register here</a></p>
           </div>
         </div>
